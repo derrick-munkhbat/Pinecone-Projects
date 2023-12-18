2036b89fcc049c6ecf3542250abf60194ec127cc
dzone24 <dulguun24@gmail.com>
2023-12-18 18:45:56 +0800

    pascal triangle

---
+++ function/index.js
@@ -0,0 +1,38 @@
+ function greet(name){
+    console.log("Hello " + name);
+    }
+    let input=prompt("Enter a name");
+    greet(input);
+
+
+function square(num){
+    return num * num;
+    }
+    var result = square(3);
+    console.log(result);
+
+
+function add(a,b) {
+    console.log(a+b);
+    }
+    add(3,4);
+        
+function triangle(x,y){
+    return x*y*0.5;
+    }
+    var result = triangle(3,4);
+    console.log(result);
+
+function formula(x){
+    return x ** 2 + 5 * x + 6;
+    }
+    var result = formula(1);
+    console.log(result);    
+
+    function formula(x){
+        return x ** 2 + 5 * x + 6;
+        }
+        var result = formula(1);
+        console.log(result);
+
+
