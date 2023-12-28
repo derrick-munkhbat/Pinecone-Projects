cc37c032f157c98821d885f2a8a2b02e9992b93a
dzone24 <dulguun24@gmail.com>
2023-12-28 18:56:58 +0800

    to-do

---
+++ DOM/select/script.js
@@ -0,0 +1,15 @@
+// 1. Use getElementById to select an element by ID
+const myElementID = document.getElementById('myElementID');
+console.log(myElementID);
+
+// 2. Use getElementsByName to select elements by name
+const myElementNames = document.getElementsByName('username');
+console.log(myElementNames);
+
+// 3. Use getElementsByTagName to select elements by tag name
+const myElementTags = document.getElementsByTagName('ul');
+console.log(myElementTags);
+
+// 4. Use getElementsByClassName to select elements by class name
+const myElementClass = document.getElementsByClassName('item');
+console.log(myElementClass);
