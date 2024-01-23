56d9ec3ae3fff3205599d2af71461ab4d5ae544b
dzone24 <dulguun24@gmail.com>
2024-01-23 19:04:55 +0800

    menu button window added

--- react/portfolio/components/Header.js
+++ react/portfolio/components/Header.js
@@ -1,18 +1,18 @@
 import { menuItems } from "@/data/menuItem";
 import { Switch } from "./icon";
 import { Logo } from "./icon/Logo";
-import { Menu } from "./icon/Menu";
 import { MainMenu } from "./MainMenu";
+import { Menu } from "./icon/Menu";
 
 
 export function Header() {
   return (
     <div className="border-2 flex justify-between p-4 items-center">
       <Logo />
+      
       <div className="flex lg:hidden">
         <Menu />
       </div>
-      
       <div className="hidden lg:flex">
         <MainMenu items={menuItems} />
         <button>< Switch /></button>
--- react/portfolio/components/MainMenu.jsx
+++ react/portfolio/components/MainMenu.jsx
@@ -1,11 +1,34 @@
+
+
 export function MainMenu({ items }) {
   return (
-    <div className="hidden lg:flex">
-      {items.map((item) => (
-        <a key={item.id} a className="px-4 py-2" href={item.link}>
-          {item.label}
-        </a>
-      ))}
+    <div>
+      <div className="hidden lg:flex">
+        {items.map((item) => (
+          <a key={item.id} a className="px-4 py-2" href={item.link}>
+            {item.label}
+          </a>
+        ))}
+      </div>
+      <MobileMenu />
+      
     </div>
+      
+
   );
 }
+
+
+function MobileMenu(){
+  const open = true;
+  return (
+  <>
+    <div className="fixed bg-slate-400/70 inset-0"></div>
+    <div className={'bg-white fixed top-0 bottom-0 w-3/4 transition-all ${open ? "right-0": "-right-full"}'}>
+      <div className="p-8 text-2xl">Mobile Menu</div>
+      <button>close</button>
+    </div>
+  </>
+  )
+}
+
