2b80ee3d9303dc13b7e96daa315b6b03655cb5e2
dzone24 <106757077+dzone24@users.noreply.github.com>
2024-03-02 17:25:18 +0800

    connected all to database

--- Expense Tracker/back-end/main.js
+++ Expense Tracker/back-end/main.js
@@ -25,23 +25,11 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
-// async function getPgVersion() {
-//   const result = await sql`select version()`;
-//   console.log(result);
-// }
-
-// getPgVersion();
-
 app.get("/", async (req, res) => {
   res.send("Hello there, backend running");
 });
 
-// app.get("/users", async (req, res) => {
-//   const result = await sql`select * from users`;
-//   res.json(result);
-// });
-
-//POST
+//USER POST
 app.post("/create-user", async (req, res) => {
   const { name, email, password, avatar_img } = req.body;
   const response =
@@ -49,25 +37,69 @@ app.post("/create-user", async (req, res) => {
   res.json(response);
 });
 
-
-//DELETE
-app.delete("/delete-user/:id", async (req, res) => {
-  const { id } = req.params;
+//TRANSACTION POST
+app.post("/create-transaction", async (req, res) => {
+  const { amount } = req.body;
   const response =
-  await sql`delete from users where id = ${id}`;
+    await sql`insert into transactions (amount) values (${amount})`;
   res.json(response);
 });
 
+//CATEGORY POST
+app.post("/create-category", async (req, res) => {
+  const { name } = req.body;
+  const response = await sql`insert into categories (name) values (${name})`;
+  res.json(response);
+});
 
-//UPDATE
+//USER DELETE
+app.delete("/delete-user/:id", async (req, res) => {
+  const { id } = req.params;
+  const response = await sql`delete from users where id = ${id}`;
+  res.json(response);
+});
+
+//TRANSACTION DELETE
+app.delete("/delete-transaction/:id", async (req, res) => {
+  const { id } = req.params;
+  const response =
+    await sql`delete from transactions where transaction_id = ${id}`;
+  res.json(response);
+});
+
+//CATEGORY DELETE
+app.delete("/delete-category/:id", async (req, res) => {
+  const { id } = req.params;
+  const response = await sql`delete from categories where category_id = ${id}`;
+  res.json(response);
+});
+
+//USER UPDATE
 app.put("/update-user/:id", async (req, res) => {
   const { id } = req.params;
   const { name, email, password, avatar_img } = req.body;
   const response =
-  await sql`update users set name = ${name}, email = ${email}, password = ${ password }, avatar_img = ${ avatar_img } where id = ${ id }`;
+    await sql`update users set name = ${name}, email = ${email}, password = ${password}, avatar_img = ${avatar_img} where id = ${id}`;
   res.json(response);
 });
 
+//TRANSACTION UPDATE
+app.put("/update-transaction/:id", async (req, res) => {
+  const { id } = req.params;
+  const { amount } = req.body;
+  const response =
+    await sql`update transactions set amount = ${amount} where transaction_id = ${id}`;
+  res.json(response);
+});
+
+//CATEGORY UPDATE
+app.put("/update-category/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const response =
+    await sql`update categories set name = ${name} where category_id = ${id}`;
+  res.json(response);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
