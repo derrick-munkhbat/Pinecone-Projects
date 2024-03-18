dfca1f147d0773546734ad527b4e026d50b01efd
derrick-munkhbat <dulguun24@gmail.com>
2024-03-18 19:37:16 +0800

    just added reload on records

--- Expense Tracker/front-end/components/RightBar.jsx
+++ Expense Tracker/front-end/components/RightBar.jsx
@@ -33,7 +33,7 @@ export function RightBar() {
       .then(() => {
         alert("success!");
         loadTask();
-        // window.location.reload();
+        window.location.reload();
       })
       .catch(() => alert("error"));
   }
