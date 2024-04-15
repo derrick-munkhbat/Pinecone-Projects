e79aa5c4b3c1738a995362c6b062acfb8391d78f
dzone24 <derrick.munkhbat@gmail.com>
2024-04-15 13:00:36 +0800

    food-delivery deleted

--- food-delivery/frontend/pages/forgot-password/createNewPassword.tsx
+++
@@ -1,5 +0,0 @@
-const createNewPassword = () => {
-  return <main>hello world, this is the createNewPassword</main>;
-};
-
-export default createNewPassword;
--- food-delivery/frontend/pages/forgot-password/resetPasswordByCode.tsx
+++
@@ -1,5 +0,0 @@
-const resetPasswordByCode = () => {
-  return <main>hello world, this is the resetPasswordByCode</main>;
-};
-
-export default resetPasswordByCode;
